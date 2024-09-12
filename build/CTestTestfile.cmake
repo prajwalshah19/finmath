c5284c3229d0e1bc72f7ec20cae2f887379ef57d
shashank524 <saishashank524@gmail.com>
2024-09-11 22:36:57 -0400

    Fixed Conflicts:

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shashank/finmath
-# Build directory: /Users/shashank/finmath/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests "/Users/shashank/finmath/build/runTests")
-set_tests_properties(UnitTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/shashank/finmath/CMakeLists.txt;33;add_test;/Users/shashank/finmath/CMakeLists.txt;0;")
-subdirs("_deps/pybind11-build")
